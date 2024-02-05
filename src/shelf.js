a0acba68effec57998889b2da1b132d0fd19523d
David Swatzell <thatsstrikethree@gmail.com>
2024-02-04 18:34:37 -0600

    finished and pass all test

--- src/shelf.js
+++ src/shelf.js
@@ -1,38 +1,36 @@
-function shelfBook(object,array){
-if (array.length < 3) {
-  array.unshift(object)
-}
-return array
-}
-
-
-function unshelfBook(bookName,array){
-for (var i =0; i < array.length; i++)
-   if (array[i].title === bookName) {
-      array.splice(i,1);
-}
+function shelfBook(object, array) {
+  if (array.length < 3) {
+    array.unshift(object);
+  }
+  return array;
 }
 
-function listTitles(shelfArray){
-var title = [] 
-for (var i = 0;i<shelfArray.length; i++){
-title.push(shelfArray[i].title)
-}
-return title.join(', ')
-}
-
-function searchShelf(shelfArray,title){
-  var titles = []
-  for (var i = 0;i<shelfArray.length;i++) {
- titles.push(shelfArray[i].title)
-return titles.includes(title)
+function unshelfBook(bookName, array) {
+  for (var i = 0; i < array.length; i++)
+    if (array[i].title === bookName) {
+      array.splice(i, 1);
+    }
 }
 
+function listTitles(shelfArray) {
+  var title = [];
+  for (var i = 0; i < shelfArray.length; i++) {
+    title.push(shelfArray[i].title);
+  }
+  return title.join(", ");
+}
 
+function searchShelf(shelfArray, title) {
+  var titles = [];
+  for (var i = 0; i < shelfArray.length; i++) {
+    titles.push(shelfArray[i].title);
+    return titles.includes(title);
+  }
+}
 
 module.exports = {
   shelfBook,
   unshelfBook,
   listTitles,
-  searchShelf
+  searchShelf,
 };
